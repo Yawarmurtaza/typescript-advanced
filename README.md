5f0d3adff8cf8d979b96a493af5a62565939fd52
Yawarmurtaza <yawar_yawar@hotmail.com>
2022-11-27 20:28:59 -0800

    tuples are working - app is okay

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # typescript-advanced
+
+## Pluralsight course link
+[Advanced TypeScript 2 by Brice Wilson](https://app.pluralsight.com/library/courses/typescript-advanced/table-of-contents)
+
+## Install typescript using node
+```
+npm install typescript --save-dev
+```
+
+## Initialise typescript
+This will creeate tsconfig.json etc files...
+```
+tsc init
+```
--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,41 @@
-console.log("Welcome!");
-console.log("Welcome 123");
+import { Category } from "./enums";
+import { Book, Logger, Author, Librarian, Magazine } from "./interfaces";
+import { UniversityLibrarian, ReferenceItem } from "./classes";
+import * as utils from "./lib/utilityFunctions";
+
+function PrintBookInfo({ title: bookTitle, author: bookAuthor }: Book): void {
+    console.log(`title: ${bookTitle} was written by ${bookAuthor}`);
+}
+
+let [b1, b2] = utils.GetAllBooks();
+// PrintBookInfo(b1);
+// PrintBookInfo(b2);
+
+function PrintBookfn([b1, b2, ...restOfTheBooks]: Book[]): void {
+    PrintBookInfo(b1);
+    PrintBookInfo(b2);
+    restOfTheBooks.forEach(b => { PrintBookInfo(b) });
+
+}
+
+// PrintBookfn(utils.GetAllBooks());
+
+let { title, author } = b1;
+//console.log(title + " - " + author);
+
+
+interface KeyValuePair<TKey, TValue> extends Array<TKey | TValue> {
+    0: TKey;
+    1: TValue;
+}
+
+let item: KeyValuePair<string, Book> = ["A 123.456", b1];
+console.log(item);
+console.log(item[0]);
+console.log(item[1]);
+
+let arr: Array<KeyValuePair<string, number>> = [
+    ["", 123],
+    ["", 123],
+    ["", 123],
+];
